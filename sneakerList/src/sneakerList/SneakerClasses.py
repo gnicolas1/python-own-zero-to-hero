b823c015fc696ecd8ad4adca5f9ce2baab3f486e
gnicolas1 <gnc4720@gmail.com>
2022-10-16 20:23:41 -0500

    SneakerList was improved and tested

--- sneakerList/src/sneakerList/SneakerClasses.py
+++ sneakerList/src/sneakerList/SneakerClasses.py
@@ -1,11 +1,11 @@
-
 class Sneaker:
-	"""Sneaker objet: brand, model, size, price"""
-	def __init__(self, brand, model, size, price):
+	"""Sneaker list object that contains a list of sneakers"""
+	def __init__(self, brand, model, size, price, gender):
 		self.brand = brand
 		self.model = model
 		self.size = size
 		self.price = price
+		self.gender = gender
 
 	def get_brand(self):
 		return self.brand
@@ -19,29 +19,88 @@ class Sneaker:
 	def get_price(self):
 		return self.price
 
-	def __str__(self):
-		pass
+	def get_gender(self):
+		return self.gender
 
 	def __repr__(self):
-		return 'Sneaker({}, {}, {}, {})'.format(self.brand, self.model, self.size, self.price)
+		return 'Sneaker({}, {}, {}, {})'.format(self.brand, self.model, self.size, self.price, self.gender)
 
 class SneakerList:
-
+	"""List of sneakers"""
 	def __init__(self):
-		pass
+		self.sneaker_list = []
+
 	def add_sneaker(self, sneaker):
-		pass
+		self.sneaker_list.append(sneaker)
+
 	def remove_sneaker(self, sneaker):
-		pass
-	def find_sneaker_by_brand(self, brand):
-		pass
-	def find_sneaker_by_model(self, model):
-		pass
-	def find_sneaker_by_size(self, model):
-		pass
-	def find_sneaker_by_price(self, model):
-		pass
-	def __str__(self):
-		pass
+		self.sneaker_list.remove(sneaker)
+
+	def filter_sneaker_by_brand(self, brand):
+		return [item for item in self.sneaker_list if item.brand == brand]
+
+	def filter_sneaker_by_model(self, model):
+		return [item for item in self.sneaker_list if item.model == model]
+
+	def filter_sneaker_by_size(self, size):
+		return [item for item in self.sneaker_list if item.size == size]
+
+	def filter_sneaker_by_price(self, price):
+		return [item for item in self.sneaker_list if item.price == price]
+
+	def filter_sneaker_by_gender(self, gender):
+		return [item for item in self.sneaker_list if item.gender == gender]
+
+	def filter_sneaker_by_attributes(self, attributes_list):
+		if (attributes_list[0][1] != '') or (attributes_list[1][1] != '') or (attributes_list[2][1] != '') or (attributes_list[3][1] != '') or (attributes_list[4][1] != ''):
+			attr_filter = []
+			if attributes_list[0][1] != '':
+				brand_f = attributes_list[0][1]
+				brand_l = self.filter_sneaker_by_brand(brand_f)
+				if len(brand_l) > 0:
+					attr_filter.append(brand_l)
+			
+			if attributes_list[1][1] != '':
+				model_f = attributes_list[1][1]
+				model_l = self.filter_sneaker_by_model(model_f)
+				if len(model_l) > 0:
+					attr_filter.append(model_l)	
+			
+			if attributes_list[2][1] != '':
+				size_f = attributes_list[2][1]
+				size_l = self.filter_sneaker_by_size(size_f)
+				if len(size_l) > 0:
+					attr_filter.append(size_l)
+			
+			if attributes_list[3][1] != '':
+				price_f = attributes_list[3][1]
+				price_l = self.filter_sneaker_by_price(price_f)
+				if len(price_l) > 0:
+					attr_filter.append(price_l)
+			
+			if attributes_list[4][1] != '':
+				gender_f = attributes_list[4][1]
+				gender_l = self.filter_sneaker_by_gender(gender_f)
+				if len(gender_l) > 0:
+					attr_filter.append(gender_l)
+
+			if len(attr_filter) == 0:
+				return "No matches were found"
+				
+			if len(attr_filter) == 1:
+				return attr_filter[0]
+
+			else:
+				set_attr = set(attr_filter[0])
+				for i in range(len(attr_filter) - 1):
+					set_attr &= set(attr_filter[i+1])
+
+				return [i for i in set_attr]
+		else:
+			return "Filter list is empty!"
+
+	def get_number_of_sneakers(self):
+		return len(self.sneaker_list)
+
 	def __repr__(self):
-		pass
+		return "Sneaker List: {}".format(self.sneaker_list)
--- sneakerList/tests/test_sneakerClasses.py
+++ sneakerList/tests/test_sneakerClasses.py
@@ -1,17 +1,82 @@
 
 import pytest
 import sys
-sys.path.append('/home/gnicolas/Programming/Python/ZeroToHero/sneakerList/src/sneakerList')
-from SneakerClasses import Sneaker
+#sys.path.append("..")
+from src.sneakerList.SneakerClasses import Sneaker, SneakerList
 
 class TestSneakerClass:
 
 	def test_sneaker_data(self):
-		snkr1 = Sneaker('Nike', 'Jordan1', 9, 150)
+		snkr1 = Sneaker('Nike', 'Jordan1', 9, 150, 'M')
 		assert snkr1.get_brand() == 'Nike'
 		assert snkr1.get_model() == 'Jordan1'
 		assert snkr1.get_size() == 9
 		assert snkr1.get_price() == 150
+		assert snkr1.get_gender() == 'M'
+
+	def test_sneaker_list(self):
+		snkr_list = SneakerList()
+		snkr_list_items_to_add = [
+			Sneaker('Nike', 'Jordan1 Retro High OG Starfish', 9, 120, 'M'),
+			Sneaker('Reebok', 'Shaq Attaq Orlando (2022)', 10.5, 210, 'M'),
+			Sneaker('Puma', 'LaMelo Ball MB.01 Galaxy', 4.5, 164, 'W'),
+			Sneaker('Adidas', 'Campus 80s South Park Towelie', 3, 129, 'W'),
+			Sneaker('Nike', 'Dunk Low Medium Olive', 3.5, 168, 'W'),
+			Sneaker('Converse', 'Run Star Motion White Black Gum', 10.5, 85, 'M'),
+			Sneaker('Nike', 'Jordan4 Retro SE Black Canvas', 12, 296, 'M'),
+			Sneaker('Reebok', 'Pump Omni Zone II Celtics', 4.5, 102, 'W'),
+			Sneaker('Adidas', 'Samba Vegan White Gum', 13.5, 108, 'M'),
+			Sneaker('Puma', 'RS-X Rick and Morty', 9, 102, 'M'),
+			Sneaker('Nike', 'Dunk Low Safari Mix', 4, 126, 'W'),
+			Sneaker('Puma', 'Suede Rhuigi Villasenor', 8.5, 84, 'M')
+		]
+
+		for item in snkr_list_items_to_add:
+			snkr_list.add_sneaker(item)
+
+		assert snkr_list.get_number_of_sneakers() == 12
+		
+		brand_list = snkr_list.filter_sneaker_by_brand("Nike")
+		assert len(brand_list) == 4
+
+		brand_list = snkr_list.filter_sneaker_by_brand("Under Armour")
+		assert len(brand_list) == 0
+
+		model_list = snkr_list.filter_sneaker_by_model("Dunk Low Safari Mix")
+		assert len(model_list) == 1
+
+		model_list = snkr_list.filter_sneaker_by_model("UA Charger Vantage 2")
+		assert len(model_list) == 0
+
+		size_list = snkr_list.filter_sneaker_by_size(9)
+		assert len(size_list) == 2
+
+		size_list = snkr_list.filter_sneaker_by_size(9.5)
+		assert len(size_list) == 0
+
+		price_list = snkr_list.filter_sneaker_by_price(102)
+		assert len(price_list) == 2
+
+		price_list = snkr_list.filter_sneaker_by_price(100)
+		assert len(price_list) == 0
+
+		gender_list = snkr_list.filter_sneaker_by_gender("W")
+		assert len(gender_list) == 5
+
+		gender_list = snkr_list.filter_sneaker_by_gender("M")
+		assert len(gender_list) == 7
+
+		attr_list = snkr_list.filter_sneaker_by_attributes([('brand', ''), ('model', ''), ('size', ''), ('price', ''), ('gender', '')])
+		assert attr_list == "Filter list is empty!"
+
+		attr_list = snkr_list.filter_sneaker_by_attributes([('brand', 'Under Armour'), ('model', ''), ('size', ''), ('price', ''), ('gender', '')])
+		assert attr_list == "No matches were found"
+
+		attr_list = snkr_list.filter_sneaker_by_attributes([('brand', ''), ('model', 'Samba Vegan White Gum'), ('size', ''), ('price', ''), ('gender', '')])
+		assert len(attr_list) == 1
+
+		attr_list = snkr_list.filter_sneaker_by_attributes([('brand', ''), ('model', ''), ('size', 9), ('price', ''), ('gender', 'M')])
+		assert len(attr_list) == 2
 
 if __name__ == "__main__":
 	pytest.main()
